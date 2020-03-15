6e447070275ea6fc00059efe0f3d3f833119ca37
Nolan Conaway <nolanbconaway@gmail.com>
2020-03-15 17:23:16 -0400

    add json encoder test

--- test/test_misc.py
+++ test/test_misc.py
@@ -1,8 +1,12 @@
 """Test utilities unrelated to any particular model."""
+import json
+
+import jax.numpy as np
 import numpy as onp
+import pytest
 
 import shabadoo
-from shabadoo.shabadoo import metrics
+from shabadoo.shabadoo import NumpyEncoder, metrics
 
 
 def test_metrics():
@@ -19,3 +23,23 @@ def test_metrics():
 def test_version():
     """Test that the version file is included."""
     assert isinstance(shabadoo.__version__, str)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        0.1,
+        1,
+        np.float32(0.1),
+        np.int32(1),
+        np.array([1, 2, 3]),
+        onp.array([1, 2, 3]),
+        np.array([[1, 2, 3], [4, 5, 6]]),
+        onp.array([[1, 2, 3], [4, 5, 6]]),
+    ],
+)
+def test_json_encoder(data):
+    """Test that the version file is included."""
+    result = json.dumps(data, cls=NumpyEncoder)
+    decoded = np.array(json.loads(result))
+    assert onp.array_equal(onp.array(json.loads(result)), onp.array(data))
