4a8f23f2a06c2ac651611198768e20373896a755
Nolan Conaway <nolanbconaway@gmail.com>
2020-07-26 11:19:40 -0400

    pin numpyro version to commit

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ from setuptools import find_packages, setup
 
 THIS_DIRECTORY = Path(__file__).resolve().parent
 
-INSTALL_REQUIRES = ["numpyro~=0.2.4", "pandas~=1.0.1", "jax~=0.1.67", "jaxlib~=0.1.47"]
+
+INSTALL_REQUIRES = [
+    # wait until numpyro makes a new release with stable deps.
+    "numpyro @ git+https://github.com/pyro-ppl/numpyro.git@e7c74e76aead421748831d5d8ce3f9b6d15df33e",
+    "pandas>=1.0.0,<2.0.0",  # ok w/ any 1.x pandas
+]
 
 # use readme as long description
 LONG_DESCRIPTION = (THIS_DIRECTORY / "readme.md").read_text()
@@ -31,7 +36,8 @@ setup(
     packages=find_packages("src"),
     install_requires=[INSTALL_REQUIRES],
     extras_require=dict(
-        test=["black", "pytest"], docs=["sphinx~=2.4.4", "m2r", "numpydoc"],
+        test=["black", "pytest", "pytest-cov", "codecov",],
+        docs=["sphinx~=2.4.4", "m2r", "numpydoc"],
     ),
     package_data={"shabadoo": ["version"]},
 )
